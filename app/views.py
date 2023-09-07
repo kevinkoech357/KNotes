e8d281fdd1c79f8628a006cbdaf13d70ef314cf7
Kevin Koech <kevinkoech@proton.me>
2023-09-07 12:46:10 +0300

    add delete note functionality. Not working as expected. Will fix later

--- app/views.py
+++ app/views.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python3
 
 # Blueprint means it has all urls defined
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
     # render_template returns the specified html
-    return render_template('home.html')
 
+    if request.method == 'POST':
+        note = request.form.get('note')
 
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(content=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added successfully!', category='success')
+
+    return render_template('home.html', user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = request.json.get('noteId')
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
